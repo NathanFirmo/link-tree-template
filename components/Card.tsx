0c8e560a5ca6f171cc1de21d9ec3ec648b3887bb
NathanFirmo <nathanssfirmo@gmail.com>
2021-12-13 19:05:36 -0300

    add cards and styles

---
+++ components/Card.tsx
@@ -0,0 +1,39 @@
+import type { NextPage } from 'next'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+interface IParams {
+  src?: string
+  description?: string
+  imgSize?: number
+  href?: string
+}
+
+const Card = (props: IParams) => {
+  return (
+    <a href={props.href || 'https://github.com/NathanFirmo'}
+      target='_blank'
+      rel='next'
+      className={styles.styledLink}>
+      <div className={styles.container}>
+        <div className={styles.item}>
+          <div className={styles.itemLogo}>
+            <Image
+              src={props.src || 'https://img.icons8.com/office/100/000000/logo.png'}
+              alt="logo"
+              width={props.imgSize || 64}
+              height={props.imgSize || 64}
+            />
+          </div>
+          <div className={styles.itemDescription || 'Description'}>
+            <p>{props.description}</p>
+          </div>
+        </div>
+      </div>
+    </a>
+  )
+}
+
+export default Card
+
+// 
